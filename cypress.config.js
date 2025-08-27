17d21ca0be8553dbf17bab7a67a6e7e08fdf9a89
gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-08-27 13:21:05 +0530

    change file

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
     supportFile: false,
-    specPattern: "cypress/e2e/**/*.cy.js",
+    specPattern: "cypress/integration/**/*.spec.js",
   },
 });
